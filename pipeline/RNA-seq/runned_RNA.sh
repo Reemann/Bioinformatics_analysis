619f3f9c71e87367425abeb262afb0bce3008cdf
Reemann <2011419@tongji.edu.cn>
2022-04-28 17:21:10 +0800

    Update runned_RNA.sh

--- pipeline/RNA-seq/runned_RNA.sh
+++ pipeline/RNA-seq/runned_RNA.sh
@@ -8,7 +8,12 @@ bash ${PRJNAXXXXXX}.sh
 
 # ----- run -----
 cd $workdir
+# normal
 RNAseq_PE_step.sh $name 1,2,3,4 $threads $genomeVersion sample${i}_rep${j}.R1.fastq.gz sample${i}_rep${j}.R2.fastq.gz <ensGene|refGene> > bin/log/${name}.log 2>&1
+# calculate expression level by Exon
+RNAseq_PE_step_ExonUniq.sh $name 1,2 $threads $genomeVersion sample${i}_rep${j}.R1.fastq.gz sample${i}_rep${j}.R2.fastq.gz <ensGene|refGene> > bin/log/${name}.log 2>&1
+# for samples having many reads & calculate expression level by Exon
+RNAseq_PE_step_ExonUniq_bigBam.sh $name 1,2 $threads $genomeVersion sample${i}_rep${j}.R1.fastq.gz sample${i}_rep${j}.R2.fastq.gz <ensGene|refGene> > bin/log/${name}.log 2>&1
 
 
 # ----- extract mapping ratio -----
@@ -19,3 +24,8 @@ RNAseq_PE_step.sh $name 1,2,3,4 $threads $genomeVersion sample${i}_rep${j}.R1.fa
 cd ${workdir}/bin
 sed -i "s/ /\t/g" sample_list.txt
 extract_log_fitCR.py sample_list.txt 'RNA-seq' data_information.csv
+
+
+# ----- view reads in IGV -----
+cd ${workdir}/2_expression_value
+samtools sort -@ $((${processer}-1)) -o ${name}_sorted.bam ${name}.bam && samtools index -@ $((${processer}-1)) ${name}_sorted.bam
