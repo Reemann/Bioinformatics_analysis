b2cbfe315bf4b6f601380cda162ebe42f59e9a9e
Reemann <2011419@tongji.edu.cn>
2022-06-04 08:01:52 +0800

    Update runned_CR.sh

--- pipeline/CUT&RUN/runned_CR.sh
+++ pipeline/CUT&RUN/runned_CR.sh
@@ -30,3 +30,21 @@ fastq_dir=path/to/fastqFiles
 blast_db=~/source/blast_db
 # the order of mapping.logs must be consistent with the alphanumeric `ls` order of fastq files
 blast_low_mapped_samples.py -d $fastq_dir -b $blast_db -n [$select_reads_number] -@ [$blast_threads] -m ${mapping_log_dir}/Mapping_${name1}.log,${mapping_log_dir}/Mapping_${name2}.log,${mapping_log_dir}/Mapping_${name3}.log
+
+
+# ----- filter peaks -----
+out_dir=${out_dir}
+qvalue_cutoff=10
+fold_cutoff=10
+
+function filter {
+    cd 3_peak
+    sample=${1}
+    awk -v fold_cutoff=${fold_cutoff} -v qvalue_cutoff=${qvalue_cutoff} 'NR>21{if($8>fold_cutoff && $9>qvalue_cutoff) printf "%s\t%d\t%d\t%s\t%d\t.\t%.5f\t%.5f\t%.5f\t%d\n", $1, $2-1, $3, $10, $9*10, $8, $7, $9, $5-$2}' ${sample}_peaks.xls > ${out_dir}/${sample}_filtered_peaks.narrowPeak
+    awk -v fold_cutoff=${fold_cutoff} -v qvalue_cutoff=${qvalue_cutoff} 'NR>21{if($8>fold_cutoff && $9>qvalue_cutoff) printf "%s\t%d\t%d\t%s\t%.5f\n", $1, $5-1, $5, $10, $9}' ${sample}_peaks.xls > ${out_dir}/${sample}_filtered_summits.bed
+    cd ../
+}
+
+cd $workdir
+filter sample
+
